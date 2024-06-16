28c26d7154f2309927827592f4e2dc8d72b8ae50
Povo <43anillo@gmail.com>
2024-06-16 21:31:20 +0000

    php整える

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!--基本情報-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+        <title>王南生徒web</title>
+        <link rel="icon" href="./favicon.ico">
+        <!--css-->
+        <link rel="stylesheet" href="./style.css">
+        <!--google icons-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    </head>
+	<body>
+            
+
+        <a href="https://forms.gle/MYthyuxwvfzbHuqc6" class="contact">
+            <img src="./contact.png" alt="contact">
+        </a>
+
+        <footer>
+          <p class="copyright">©Kogo Takemura</p>
+        </footer>
+    </body>
+</html>
